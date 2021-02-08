cd65e91f5011cd414aef4b9ee446fcd161baaa9b
Farrelhasyidan <farrelhasyidan@gmail.com>
2021-02-08 14:14:19 +0700

    Project finished

---
+++ NewsUI/Model/News.swift
@@ -0,0 +1,14 @@
+//
+//  News.swift
+//  NewsUI
+//
+//  Created by Farrel hasyidan on 08/02/21.
+//
+
+import SwiftUI
+struct News : Identifiable {
+    var id = UUID()
+    var title : String
+    var image : String
+    var description : String
+}
